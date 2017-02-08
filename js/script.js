8c4cdfe27b31150468cb10b61eab7748b6926638
SunyoungSH <email@seiler-hwang.de>
2017-02-08 14:22:41 +0100

    added wiki api

--- js/script.js
+++ js/script.js
@@ -20,7 +20,7 @@ function loadData() {
     var streetviewUrl = 'http://maps.googleapis.com/maps/api/streetview?size=600x400&location='+address;
     $body.append('<img class="bgimg" src="' + streetviewUrl + '">');
 
-    var nytUrl = "";
+    var nytUrl = 'http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+cityStr+'&sort=newest&api-key=8d8b437dc9e94dc2a9d342a548c2b517';
     $.getJSON(nytUrl, function(data) {
       $nytHeaderElem.text('New York Times articles about '+cityStr);
       articles = data.response.docs;
@@ -32,6 +32,20 @@ function loadData() {
       $nytHeaderElem.text('Page could not be loaded');
     })
 
+    var wikiUrl = 'http://en.wikipedia.org/w/api.php?action=opensearch&search='+cityStr+'&format=json&callback=wikiCallback';
+    $.ajax({
+      url: wikiUrl,
+      data: queryData,
+      dataType: 'jsonp',
+      done: function(data){
+        var articleList = data[1];
+        for(i = 0; i<articleList.length; i++){
+        articleStr = articleList[i];
+        var url='http://en.wikipedia.org/wiki/'+articleStr;
+        $wikiElem.append('<li><a href="+url+">'+articleStr+'</a></li>');
+      }
+      }
+    })
     return false;
 };
 
